750db282892cb7a6154692ded9fdadea6aa75d36
Saidev Dhal <connect.saidev@gmail.com>
2024-08-14 18:21:42 +0530

    chore: Just a small fix

--- db/func.ts
+++ db/func.ts
@@ -2,16 +2,17 @@ import { OAuthProvider } from "appwrite";
 import { AppwriteUser } from "./init";
 
 async function GoogleSignIn() {
-  const urlCallback = process.env.NEXT_PUBLIC_URL!
+  const isDevelopment = process.env.NODE_ENV === "development";
   try {
     AppwriteUser.createOAuth2Session(
       OAuthProvider.Google,
-      `${urlCallback}/chat`, // success
-    );    
+      isDevelopment
+        ? "http://localhost:3000/chat"
+        : "https://x0-gpt.devwtf.in/chat"
+    );
   } catch (error) {
     console.error("ERROR OCCURED WHILE LOGGING IN:", error);
   }
 }
-
 
 export { GoogleSignIn };
